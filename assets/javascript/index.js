8b570c53f8bb77c62ff1c3d304f74ead5625dbd3
votrieuvy1661 <votrieuvy1661@gmail.com>
2022-03-16 21:07:04 +0700

    css scroll color

---
+++ assets/javascript/index.js
@@ -0,0 +1,36 @@
+$(window).on("scroll", function () {
+    var pos = $(window).scrollTop();
+    var pos2 = pos + 50;
+    // Link Highlighting
+    if (pos2 > $("#home").offset().top) {
+        highlightLink("home");
+    }
+    if (pos2 > $("#about").offset().top) {
+        highlightLink("about");
+    }
+    if (pos2 > $("#portfolio").offset().top) {
+        highlightLink("portfolio");
+    }
+    if (
+        pos2 > $("#contact").offset().top ||
+        pos + $(window).height() === $(document).height()
+    ) {
+        highlightLink("contact");
+    }
+    // Prevent Hover on Scroll
+    // clearTimeout(lockTimer);
+    // if (!$("body").hasClass("disable-hover")) {
+    //     $("body").addClass("disable-hover");
+    // }
+
+    // lockTimer = setTimeout(function () {
+    //     $("body").removeClass("disable-hover");
+    // }, 500);
+});
+
+function highlightLink(anchor) {
+    $("nav .active").removeClass("active");
+    $("nav")
+        .find('[href="#' + anchor + '"]')
+        .addClass("active");
+}
